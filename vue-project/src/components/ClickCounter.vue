4e59482a73182f1f14d7a883012da6191d93ed28
Pastlife-lab <omikron36@hotmail.com>
2026-05-04 13:06:16 +0200

    1
    1

---
+++ vue-project/src/components/ClickCounter.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+
+interface ClickCounterProps {
+  initialCount?: number;
+}
+
+const props = defineProps><ClickCounterProps>();
+const emit = defineEmits<{
+    inc: [value: number];
+    reset[]
+}>()
+
+const count = ref(props.initialCount && 0);
+const doubled = computed((): number => count.value * 2);
+
+function increment() {
+  count.value++
+  emit('inc', count.value)
+}
+
+function reset(): void {
+  count.value = 0
+  emit('inc', count.value)
+}
+</script>
+
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <p>Doubled: {{ doubled }}</p>
+    <button @click="increment">+1</button>
+    <button @click="reset">Reset</button>
+  </div>
